cc6c8de81882e3af2eed26992d6b82100fedd75a
zeynalovva <zeynalovvabbas@gmail.com>
2025-04-13 13:03:48 +0400

    Jump Game, Jump Game II

---
+++ src/com/zeynalovv/leetcode/DP/JumpGame.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+
+    bool canJump(vector<int>& nums) {
+        if(nums.size() == 1){
+            return true;
+        }
+        vector<int>arr(nums.size());
+        for(int i = nums.size()-2; i>=0;i--){
+            int h = nums[i];
+            h+=i;
+            if(h>=nums.size()-1){
+                arr[i] = 1;
+            }
+            else{
+                int mn = INT_MAX, index = i;
+                for(int j = i + 1;j<=h;j++){
+                    if(arr[j] != 0){
+                        mn = min(mn, arr[j]);
+                    }
+                }
+                if(mn == INT_MAX){
+                    arr[i] = 0;
+                }
+                else{
+                    arr[i] = mn + 1;
+                }
+            }
+        }
+        if(arr[0] == 0) return false;
+        return true;
+    }
+};
